a131491951544c6abdfde877c2b9461393a51b2c
minjunsong <songmj5000@naver.com>
2025-09-22 13:01:49 +0900

    feat: 스프링 부트 서버에 리액트 에서 오는 요청을 허용하기 위해 cors 문제 해결

---
+++ src/main/java/com/example/GoodPlace/config/WebConfig.java
@@ -0,0 +1,30 @@
+package com.example.GoodPlace.config;
+
+import com.example.GoodPlace.config.auth.LoginUserArgumentResolver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private final LoginUserArgumentResolver loginUserArgumentResolver;
+    
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(loginUserArgumentResolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")  // api로 시작하는 모든 경로
+                .allowedOrigins("http://localhost:3000")    // 리엑트 앱 주소
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
+                .allowCredentials(true);    // 쿠키 인증 요청 허용
+    }
+}
